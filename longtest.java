3d649f76caa6540f48e77f3f6450fbe81c36f43d
ivansnow02 <ivansnow02@gmail.com>
2022-09-10 16:11:46 +0800

    tested "long" and "char"

---
+++ longtest.java
@@ -0,0 +1,9 @@
+public class longtest {
+    public static void main(String[] args) {
+        long a = 9999999999L;//must add "L"
+        System.out.println(a);
+        char c = '众';//characters could be used
+        System.out.println(c);
+    }
+    
+}
