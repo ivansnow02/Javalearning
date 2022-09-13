f5e7ee3ea837030142fdaa994e8733e77967e4b1
ivansnow02 <ivansnow02@gmail.com>
2022-09-13 22:23:31 +0800

    冒泡排序

---
+++ Bubblesort.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Bubblesort{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int a[] = new int[5];
+        for (int i = 0 ; i < 5 ; i++){
+            a[i] = input.nextInt();
+        }
+        Bubblesort sorter = new Bubblesort();
+        sorter.sort(a);
+
+    }
+
+    public void sort(int[] a){
+        for (int i = 0 ; i < a.length ; i++){
+            for (int j = 1 ; j < a.length - 1 ; j++){
+                if (a[j] > a[j + 1]){
+                    a[j] ^= a[j + 1];
+                    a[j + 1] ^= a[j];
+                    a[j] ^= a[j + 1];
+
+                }
+            }
+        }
+        for (int i : a){
+            System.out.printf("%d ",i);
+        }
+    }
+}
