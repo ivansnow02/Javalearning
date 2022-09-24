6d5612a87e1b99b08e61411961ce232c526a0979
ivansnow02 <ivansnow02@gmail.com>
2022-09-24 11:30:07 +0800

    练习7.06

---
+++ Valutest.java
@@ -0,0 +1,11 @@
+public class Valutest {
+    private int value;
+
+    public void setvalue(int x) {
+        this.value = x;
+    }
+
+    public int getvalue() {
+        return this.value;
+    }
+}
---
+++ test706.java
@@ -0,0 +1,8 @@
+public class test706 {
+    
+    public static void main(String[] args) {
+        Valutest test = new Valutest();
+        test.setvalue(2022);
+        System.out.println(test.getvalue());
+    }
+}
