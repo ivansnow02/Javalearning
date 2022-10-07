6de7c09f41affaa614380921d73ee6fc15035ac6
ivansnow02 <ivansnow02@gmail.com>
2022-10-07 23:20:01 +0800

    把内层循环修正了

--- Bubblesort.java
+++ Bubblesort.java
@@ -14,7 +14,7 @@ public class Bubblesort {
 
     public void sort(int[] a) {
         for (int i = 0; i < a.length; i++) {
-            for (int j = 1; j < a.length - 1; j++) {
+            for (int j = 0; j < a.length - 1; j++) {
                 if (a[j] > a[j + 1]) {
                     a[j] ^= a[j + 1];
                     a[j + 1] ^= a[j];
